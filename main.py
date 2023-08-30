d1d9d193812da64c70f5b77836485c045a89b66a
Erfan Nasri <erfannasri2@gmail.com>
2023-08-30 03:49:28 +0330

    Add option user to be able to set a specific text for each channel

--- main.py
+++ main.py
@@ -1,52 +1,64 @@
 import logging
-import os
 
-from dotenv import load_dotenv
-from telegram.ext import Filters, MessageHandler, Updater
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-load_dotenv()
-# Retrieve values from environment variables
-TOKEN = os.getenv('BOT_TOKEN')
-ADDITIONAL_TEXT = os.getenv('ADDITIONAL_TEXT')
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+CHANNEL_TEXT_MAP = {}
 
-if TOKEN is None:
-    raise ValueError("BOT_TOKEN environment variable is not set")
-if ADDITIONAL_TEXT is None:
-    raise ValueError("ADDITIONAL_TEXT environment variable is not set")
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+
+def start(update, context):
+    message = update.effective_message
+    message.reply_text("Welcome to the robot! Please use the /setinfo command to set the channel ID and text.\nTo find the channel ID, just send a message from your channel to the @get_id_bot bot.\n\n\nExample:\n/setinfo -100... yourText")
+
+
+def set_info(update, context):
+    message = update.effective_message
+    chat_id = message.chat_id
+    text = message.text.split('/setinfo', 1)[1].strip()
+
+    try:
+        channel_id, additional_text = text.split(maxsplit=1)
+    except ValueError:
+        message.reply_text('Invalid input. Please provide the channel ID and additional text separated by a space.')
+        return
+
+    CHANNEL_TEXT_MAP[channel_id] = additional_text
+
+    message.reply_text(f'Channel ID and additional text set successfully. Channel ID: {channel_id}, '
+                       f'Additional text: {additional_text}')
 
 
 def add_text_to_message(update, context):
     message = update.effective_message
+    chat_id = str(message.chat_id)
 
-    if message.photo:
-        caption = message.caption if message.caption else ''
-        caption += ADDITIONAL_TEXT
-        context.bot.edit_message_caption(chat_id=message.chat_id, message_id=message.message_id, caption=caption)
-    elif message.video:
-        caption = message.caption if message.caption else ''
-        caption += ADDITIONAL_TEXT
-        context.bot.edit_message_caption(chat_id=message.chat_id, message_id=message.message_id, caption=caption)
-    elif message.audio:
-        caption = message.caption if message.caption else ''
-        caption += ADDITIONAL_TEXT
-        context.bot.edit_message_caption(chat_id=message.chat_id, message_id=message.message_id, caption=caption)
-    elif message.animation:
-        caption = message.caption if message.caption else ''
-        caption += ADDITIONAL_TEXT
-        context.bot.edit_message_caption(chat_id=message.chat_id, message_id=message.message_id, caption=caption)
-    else:
-        # No media type specified, add text to text message
-        edited_text = message.text + ADDITIONAL_TEXT
-        context.bot.edit_message_text(chat_id=message.chat_id, message_id=message.message_id, text=edited_text)
+    if chat_id in CHANNEL_TEXT_MAP:
+        additional_text = CHANNEL_TEXT_MAP[chat_id]
+
+        if message.photo:
+            caption = message.caption if message.caption else ''
+            caption += '\n\n' + additional_text
+            context.bot.edit_message_caption(chat_id=message.chat_id, message_id=message.message_id, caption=caption)
+        elif message.video:
+            caption = message.caption if message.caption else ''
+            caption += '\n\n' + additional_text
+            context.bot.edit_message_caption(chat_id=message.chat_id, message_id=message.message_id, caption=caption)
+        elif message.audio:
+            caption = message.caption if message.caption else ''
+            caption += '\n\n' + additional_text
+            context.bot.edit_message_caption(chat_id=message.chat_id, message_id=message.message_id, caption=caption)
 
 
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('setinfo', set_info))
     dp.add_handler(MessageHandler(Filters.all, add_text_to_message))
+
     updater.start_polling()
     updater.idle()
 
